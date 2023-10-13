bef53ab28b5983db60bc69e276ebb8ba3f81810c
Petr Brantalík <grounipteam@gmail.com>
2023-10-13 17:36:26 +0200

    chore: cmp

--- lua/grounip/plugins.lua
+++ lua/grounip/plugins.lua
@@ -48,6 +48,18 @@ return packer.startup(function(use)
 	-- Colorschemes
   use "lunarvim/darkplus.nvim"
 
+		-- Cmp
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+	use "saadparwaiz1/cmp_luasnip" -- snippet completions
+	use "hrsh7th/cmp-nvim-lsp"
+	use "hrsh7th/cmp-nvim-lua"
+
+		-- Snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
