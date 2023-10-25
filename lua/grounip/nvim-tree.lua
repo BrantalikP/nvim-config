f8e2c4dc6376571b56f121041e31fb623e10614b
Petr Brantalík <48878547+BrantalikP@users.noreply.github.com>
2023-10-25 10:25:21 +0200

    chore: adjusted mapping (#19)
    Co-authored-by: Petr Brantalík <grounipteam@gmail.com>

--- lua/grounip/nvim-tree.lua
+++ lua/grounip/nvim-tree.lua
@@ -3,15 +3,26 @@ if not status_ok then
   return
 end
 
--- TODO: Need to find an alternative
--- local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
--- if not config_status_ok then
---   return
--- end
---
--- local tree_cb = nvim_tree_config.nvim_tree_callback
+
+
+-- CUSTOM MAPPINGS https://github.com/nvim-tree/nvim-tree.lua/blob/master/doc/nvim-tree-lua.txt
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+  vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+end
 
 nvim_tree.setup {
+	on_attach = my_on_attach,
   update_focused_file = {
     enable = true,
     update_cwd = true,
@@ -56,13 +67,6 @@ nvim_tree.setup {
   },
   view = {
     width = 30,
-    side = "left",
-    -- mappings = {
-      -- list = {
-        -- { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        -- { key = "h", cb = tree_cb "close_node" },
-        -- { key = "v", cb = tree_cb "vsplit" },
-      -- },
-    -- },
+    side = "left"
   },
 }
