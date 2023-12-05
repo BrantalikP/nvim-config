c44f3d2acf18a14ef50f775c597b4a5faf69a850
Petr Brantalík <48878547+BrantalikP@users.noreply.github.com>
2023-12-05 06:08:24 +0100

    GO (#32)
    * chore: go lsp
    
    * chore: autosave + fixing copilot
    
    * go
    
    ---------
    
    Co-authored-by: Petr Brantalík <grounipteam@gmail.com>

--- lua/grounip/lsp/null-ls.lua
+++ lua/grounip/lsp/null-ls.lua
@@ -8,12 +8,33 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-    -- diagnostics.flake8
+		formatting.gofumpt,
+		formatting.goimports_reviser,
+		formatting.golines,
+		-- diagnostics.flake8
 	},
+	-- autosave
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({
+				group = augroup,
+				buffer = bufnr,
+			})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
